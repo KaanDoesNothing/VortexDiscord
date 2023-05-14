e4563c6e7d71fbd161a5b3bc4037834a6caee12d
KaanDoesNothing <kaanyt123@gmail.com>
2023-05-14 18:32:40 +0200

    added port

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
         name: "VortexFrontend",
         exec_mode: "cluster",
         instances: "max",
+        port: 7807,
         script: "./.output/server/index.mjs"
       }
     ]
