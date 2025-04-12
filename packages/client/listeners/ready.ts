47d2a9e8c92b06cce455ae397c9895a8130b1189
KaanDoesNothing <kaanyt123@gmail.com>
2025-04-13 00:01:57 +0200

    Fix precense

--- packages/client/listeners/ready.ts
+++ packages/client/listeners/ready.ts
@@ -12,5 +12,7 @@ export class ReadyListener extends Listener {
 
     public async run() {
         console.log(`Shard ${this.container.client.guilds.cache.at(0)?.shardId} - Ready`);
+
+        await this.container.client.user!.setPresence({activities: [{name: `=>help | ${this.container.client.guilds.cache.size} Servers`, url: `https://www.twitch.tv/${this.container.client.user!.username}`, type: 1}]});
     }
 }
